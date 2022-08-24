6a7c861e11f31e19d74f3e1d9dc8f3ad539b0f3c
hoejun <hjkim.it@gmail.com>
2022-08-24 14:14:11 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import Header from './components/Header';
 import ContentRouter from './components/routers/ContentRouter';
 import AppTemplate from './components/AppTemplate';
 
-// 테스트테스트
-// 테스트테스트
 const App = () => {
   return <AppTemplate Header={<Header />} Content={<ContentRouter />} />;
 };
