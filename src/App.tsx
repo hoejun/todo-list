fba811491d1d77a8f53838fb8279497e456d1d0b
hoejun <hjkim.it@gmail.com>
2022-08-25 15:30:54 +0900

    파이어 베이스 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/todo.css';
 import './styles/edit.css';
 import Header from './components/Header';
 import ContentRouter from './components/routers/ContentRouter';
 import AppTemplate from './components/templates/AppTemplate';
-//테스트
+import { initializeApp } from 'firebase/app';
+import { getFirestore, getDoc, doc } from 'firebase/firestore';
+import { firebaseConfig } from './config/config';
+
+const _firebaseConfig = firebaseConfig;
+const app = initializeApp(_firebaseConfig);
+const db = getFirestore(app);
+
 const App = () => {
+  const getDate = async () => {
+    const docRef = doc(db, 'todos', 'todo-fire');
+    const docSnap = await getDoc(docRef);
+    // console.log(docSnap.data());
+  };
+  useEffect(() => {
+    getDate();
+  }, []);
   return <AppTemplate Header={<Header />} Content={<ContentRouter />} />;
 };
 
