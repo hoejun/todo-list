81b6a569b67996d21fade119a7a3d7d564617614
HoeJun Kim <hjkim.it@gmail.com>
2022-10-27 23:57:49 +0900

    테스트 파이어 베이스 주석

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,23 @@ import './styles/edit.css';
 import Header from './components/Header';
 import ContentRouter from './components/routers/ContentRouter';
 import AppTemplate from './components/templates/AppTemplate';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, getDoc, doc } from 'firebase/firestore';
-import { firebaseConfig } from './config/config';
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore, getDoc, doc } from 'firebase/firestore';
+// import { firebaseConfig } from './config/config';
 
-const _firebaseConfig = firebaseConfig;
-const app = initializeApp(_firebaseConfig);
-const db = getFirestore(app);
+// const _firebaseConfig = firebaseConfig;
+// const app = initializeApp(_firebaseConfig);
+// const db = getFirestore(app);
 
 const App = () => {
-  const getDate = async () => {
-    const docRef = doc(db, 'todos', 'todo-fire');
-    const docSnap = await getDoc(docRef);
-    // console.log(docSnap.data());
-  };
-  useEffect(() => {
-    getDate();
-  }, []);
+  // const getDate = async () => {
+  //   const docRef = doc(db, 'todos', 'todo-fire');
+  //   const docSnap = await getDoc(docRef);
+  //   // console.log(docSnap.data());
+  // };
+  // useEffect(() => {
+  //   getDate();
+  // }, []);
   return <AppTemplate Header={<Header />} Content={<ContentRouter />} />;
 };
 
