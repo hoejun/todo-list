8873f4e31b6f1d8e6a3ad8b3fa992ed10c48c5f0
hoejun <hjkim.it@gmail.com>
2022-11-11 16:00:48 +0900

    refactor:코드정리

--- src/components/contents/HomeContent.tsx
+++ src/components/contents/HomeContent.tsx
@@ -37,31 +37,15 @@ const fetchWeather = async () => {
 const HomeContent = ({ history }: { history: any }) => {
   const [content, setContent] = useRecoilState<any>(listState);
   const [dailyWeather, setDailyWeather] = useState<Data[]>([]);
-  const [loading, setLoading] = useState(true);
   const { isLoading, error, data }: any = useQuery(['weatherKey'], fetchWeather);
   //isLoading은 데이터가 없을경우만 true
   // if (isLoading) return <div> 로딩중... </div>;
   // if (error) return <div> 에러: {error.message} </div>;
 
-  // if (!isLoading) {
-  //   let array: Data[] = [];
-  //   data.data.daily.forEach((e: any) => {
-  //     const items = {
-  //       id: e.dt,
-  //       date: UnixToTimeStamp(e.dt),
-  //       icon: e.weather[0].icon,
-  //       temperature: e.temp.day,
-  //     };
-  //     array.push(items);
-  //   });
-  //   setDailyWeather(array);
-  // }
   const getData = async () => {
-    const weatherData: any = await API.getWeather();
     let array: Data[] = [];
-    // console.log(data);
-    weatherData.data.daily.forEach((e: any) => {
-      // data.data.daily.forEach((e: any) => {
+
+    data?.daily.forEach((e: any) => {
       const items = {
         id: e.dt,
         date: UnixToTimeStamp(e.dt),
@@ -71,7 +55,6 @@ const HomeContent = ({ history }: { history: any }) => {
       array.push(items);
     });
     setDailyWeather(array);
-    setLoading(false);
   };
   const handleClick = (e: any) => {
     setContent(
@@ -90,20 +73,7 @@ const HomeContent = ({ history }: { history: any }) => {
   };
 
   useEffect(() => {
-    // getData();
-    if (data) {
-      let array: Data[] = [];
-      data?.daily.forEach((e: any) => {
-        const items = {
-          id: e.dt,
-          date: UnixToTimeStamp(e.dt),
-          icon: e.weather[0].icon,
-          temperature: e.temp.day,
-        };
-        array.push(items);
-      });
-      setDailyWeather(array);
-    }
+    if (data) getData();
   }, [data]);
   useEffect(() => {
     window.localStorage.setItem('todoList', JSON.stringify(content));
@@ -112,7 +82,6 @@ const HomeContent = ({ history }: { history: any }) => {
   return (
     <div>
       <div className='weather_content'>
-        {console.log(dailyWeather)}
         <Weather dailyWeather={dailyWeather} loading={isLoading} />
       </div>
       <div className='to_do_content' style={{ marginTop: '30px' }}>
